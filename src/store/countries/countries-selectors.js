3928e33d4bea643df016235eb9093ef5947a5b5a
Vladislav <vladgm233223@gmail.com>
2023-01-10 23:33:45 +0200

    filter by search
    добавил фильтрацию стран по строке поиска

--- src/store/countries/countries-selectors.js
+++ src/store/countries/countries-selectors.js
@@ -1,7 +1,15 @@
 export const selectCountriesInfo = (state) => ({
-    status: state.countries.status,
-    error: state.countries.error,
-    qty: state.countries.list.length
-})
+  status: state.countries.status,
+  error: state.countries.error,
+  qty: state.countries.list.length,
+});
 
-export const selectAllCountries = (state) => state.countries.list
+export const selectAllCountries = (state) => state.countries.list;
+
+export const selectVisibleCountries = (state, { search = "" }) => {
+  return state.countries.list.filter(
+    (country) => (
+        country.name.toLowerCase().includes(search.toLowerCase())
+    )
+  )
+}
--- src/store/root-reduser.js
+++ src/store/root-reduser.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import {themeReduser} from "./theme/theme-reduser"
 import { countriesReduser } from "./countries/countries-reduser";
+import { controlsReduser } from "./controls/controls-reduser";
 
 export const rootReduser = combineReducers({
     theme: themeReduser,
     countries: countriesReduser,
+    controls: controlsReduser
 })
