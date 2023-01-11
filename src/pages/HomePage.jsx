54b33b87c99ddd7b8878adeff3229903a9c25852
Vladislav <vladgm233223@gmail.com>
2023-01-11 12:11:07 +0200

    filter by region
    добавил фильтрацию по региону, при изменении строки из выпадающего списка

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,31 @@
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-import { List } from '../components/List';
-import { Card } from '../components/Card';
-import { Controls } from '../components/Controls';
-import { loadCountries } from '../store/countries/countries-actions';
+import { List } from "../components/List";
+import { Card } from "../components/Card";
+import { Controls } from "../components/Controls";
+import { loadCountries } from "../store/countries/countries-actions";
 
-import { selectCountriesInfo, selectVisibleCountries} from "../store/countries/countries-selectors"
-import { selectSearch } from '../store/controls/controls-selectors';
+import {
+  selectCountriesInfo,
+  selectVisibleCountries,
+} from "../store/countries/countries-selectors";
+import { selectControls } from "../store/controls/controls-selectors";
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
-  const dispatch = useDispatch()
-  const search = useSelector(selectSearch)
-  const countries = useSelector((state) => selectVisibleCountries(state, {search}));
-  const {status, error, qty} = useSelector(selectCountriesInfo)
-
-  
+  const dispatch = useDispatch();
+  const { search, region } = useSelector(selectControls);
+  const countries = useSelector((state) =>
+    selectVisibleCountries(state, { search, region })
+  );
+  const { status, error, qty } = useSelector(selectCountriesInfo);
 
   useEffect(() => {
-    !qty && dispatch(loadCountries())
-  } , [qty, dispatch])
+    !qty && dispatch(loadCountries());
+  }, [qty, dispatch]);
 
   return (
     <>
@@ -32,37 +35,36 @@ export const HomePage = () => {
       {status === "loading" && <h2>Loading...</h2>}
       {status === "received" && (
         <List>
-        {countries.map((c) => {
-          const countryInfo = {
-            img: c.flags.png,
-            name: c.name,
-            info: [
-              {
-                title: 'Population',
-                description: c.population.toLocaleString(),
-              },
-              {
-                title: 'Region',
-                description: c.region,
-              },
-              {
-                title: 'Capital',
-                description: c.capital,
-              },
-            ],
-          };
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: "Population",
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: c.region,
+                },
+                {
+                  title: "Capital",
+                  description: c.capital,
+                },
+              ],
+            };
 
-          return (
-            <Card
-              key={c.name}
-              onClick={() => navigate(`/country/${c.name}`)}
-              {...countryInfo}
-            />
-          );
-        })}
-      </List>
+            return (
+              <Card
+                key={c.name}
+                onClick={() => navigate(`/country/${c.name}`)}
+                {...countryInfo}
+              />
+            );
+          })}
+        </List>
       )}
-      
     </>
   );
 };
--- src/store/controls/controls-selectors.js
+++ src/store/controls/controls-selectors.js
@@ -1,1 +1,8 @@
 export const selectSearch = (state) => state.controls.search
+
+export const selectRegion = (state) => state.controls.region 
+
+export const selectControls = (state) => state.controls
+
+
+
